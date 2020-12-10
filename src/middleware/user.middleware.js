05025dd22e55a004aa227c07f78dcdb0fe2370d2
Hypeng <haoyp1@lenovo.com>
2020-12-10 19:08:36 +0800

    day2

---
+++ src/middleware/user.middleware.js
@@ -0,0 +1,21 @@
+const errorTypes =require('../constants/error-types')
+const service = require('../service/user.service');
+const verifyUser = async (ctx,next)=>{
+    //1.获取用户名密码
+    const {name,password} = ctx.request.body;
+    //2判断用户名密码不能为空
+    if(!name||!password){
+        const error = new Error(errorTypes.NAME_OR_PASSWORD_IS_REQUIRED)
+        return ctx.app.emit('error',error,ctx)
+    }
+    //3.判断这次注册的用户名是否被注册过
+    const result = await service.getUserByName(name)
+    if(result.length){
+        const error = new Error(errorTypes.USER_ALREADY_EXISTS);
+        return ctx.app.emit('error',error,ctx)
+    }
+    await next()
+}
+module.exports={
+    verifyUser
+}
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,10 +1,18 @@
-const connection 
+const connection = require('../app/database')
 class UserService {
     async create(user) {
         //将user存储到数据库中 
         const {name,password} = user;
         const statement = `INSERT INTO users (name,password) VALUES (?,?)`
-        return "创建用户成功"
+
+        const result = await connection.execute(statement,[name,password])
+        return result[0]
+    }
+    async getUserByName(name) {
+        const statement = `SELECT * FROM users WHERE name=?`;
+        const result =await connection.execute(statement,[name])
+        console.log(result);
+        return result[0]
     }
 }
 module.exports = new UserService()
